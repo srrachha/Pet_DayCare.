23c67b67054f6134521a22ffc85528f56c7aba9a
ssrachha <sharayurachha06@gmail.com>
2026-04-23 21:48:13 +0530

    Fix Express 5 wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const distPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(distPath));
 
 // Catch-all route to serve Frontend index.html for SPA routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
